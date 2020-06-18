f9031924e92c9b5431956971f52d5f3cb6146a85
Felix MIL <felixmil@outlook.com>
2020-06-18 18:14:48 +0200

    added function to compute result fromm full roll command

---
+++ R/evaluate_roll_cmd.R
@@ -0,0 +1,22 @@
+#' Title
+#'
+#' @param parsed_cmd
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evaluate_roll_cmd <- function(parsed_cmd){
+  final_result <- NA
+
+  for (i in 1:length(parsed_cmd$elements)) {
+    result <- roll_one(parsed_cmd$elements[i],parsed_cmd$operators[i-1])
+    if (is.na(final_result)) {
+      final_result = result
+    } else {
+      final_result = eval(parse(text = paste(final_result, parsed_cmd$operators[i-1], result)))
+    }
+  }
+
+  return(final_result)
+}
