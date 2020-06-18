e20c86761e70326f96bbc48f3cab77ceb62a9c6d
Felix MIL <felixmil@outlook.com>
2020-06-18 23:48:47 +0200

    updated tests

--- tests/testthat/test-roll_dice.R
+++ tests/testthat/test-roll_dice.R
@@ -5,8 +5,28 @@ test_that("roll dice with addition works", {
   expect_true(roll == 2)
 })
 
+test_that("roll with substraction", {
+  set.seed(42)
+  roll = roll_dice("1d100-20")
+  expect_true(roll == 29)
+})
+
 test_that("addition of rolls works", {
   set.seed(42)
   roll = roll_dice("1d4+1d6+1d8+1d10")
   expect_true(roll == 16)
 })
+
+test_that("substraction of rolls works", {
+  set.seed(42)
+  roll = roll_dice("1d100-1d20")
+  expect_true(roll == 44)
+})
+
+test_that("print history works",{
+  set.seed(42)
+  expect_message(roll_dice("1d100",roll_history = TRUE))
+})
+
+
+
--- tests/testthat/test-roll_one.R
+++ tests/testthat/test-roll_one.R
@@ -1,9 +1,28 @@
-set.seed(42)
-
 test_that("simple roll works", {
+  set.seed(42)
   roll1 = roll_one("1d6")
   roll2 = roll_one("1d6")
 
   expect_true(roll1 == 1)
   expect_true(roll2 == 5)
 })
+
+test_that("keep highest works",{
+  set.seed(42)
+  roll = roll_one("10d10h2")
+  expect_equal(roll, 20)
+  })
+
+test_that("keep lowest works",{
+  set.seed(42)
+  roll = roll_one("10d10l2")
+  expect_equal(roll, 2)
+})
+
+
+
+test_that("warning works",{
+  set.seed(42)
+  expect_warning(roll_one("wrong_syntax"))
+})
+
