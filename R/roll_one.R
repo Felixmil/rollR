c5eb02238fda8df1056e79cb239889ea9ee30f8d
Felix MIL <felixmil@outlook.com>
2020-06-18 23:47:50 +0200

    all patterns and result computation functions are put together in a list.  - roll pattern recognition evaluate elements of list 1 by one until one is found.

--- R/roll_one.R
+++ R/roll_one.R
@@ -1,42 +1,60 @@
-#' Roll a dice command
+#' Roll a die from text command
 #'
-#' @param dice a string corresponding to a dice command
+#' @param roll a string corresponding to a roll command
 #'
 #' @return result of the roll(s)
 #'
 #' @examples
-roll_one <- function(dice, operator){
-  no_dice_pattern = "^\\d+$"
-  simple_pattern = "^(\\d+)[dD](\\d+)$"
-  keep_h_pattern = "^(\\d+)[dD](\\d+)[Hh](\\d+)"
-  keep_l_pattern = "^(\\d+)[dD](\\d+)[Ll](\\d+)"
-
-  if (!is.na(stringr::str_match(dice, no_dice_pattern)[1])) {
-    result = stringr::str_match(dice, no_dice_pattern)[1]
-    message(paste0("Adding ",operator, result))
-  } else if (!is.na(stringr::str_match(dice, simple_pattern)[1])) {
-    match = stringr::str_match(dice, simple_pattern)
-    message(paste0("Rolling ", match[2],'d',match[3],':'))
-    rolls = sample(1:match[3],match[2], replace = TRUE)
-    message(paste('\t',paste(rolls, collapse = ', ')))
-    result = sum(rolls)
-  } else if (!is.na(stringr::str_match(dice, keep_h_pattern)[1])) {
-    match = stringr::str_match(dice, keep_h_pattern)
-    message(paste0("Rolling ", match[2],'d',match[3],':'))
-    rolls = sample(1:match[3],match[2], replace = TRUE)
-    message(paste('\t',paste(rolls, collapse = ', ')))
-    message(paste("keeping", match[4], "highest roll(s):", paste(sort(rolls,decreasing = T)[as.numeric(1:match[4])], collapse=", ")))
-    result =  sum(sort(rolls,decreasing = T)[1:as.numeric(match[4])])
-  } else if (!is.na(stringr::str_match(dice, keep_l_pattern)[1])) {
-    match = stringr::str_match(dice, keep_l_pattern)
-    message(paste0("Rolling ", match[2],'d',match[3],':'))
-    rolls = sample(1:match[3],match[2], replace = TRUE)
-    message(paste('\t',paste(rolls, collapse = ', ')))
-    message(paste("keeping", match[4], "lowest roll(s):", paste(sort(rolls)[as.numeric(1:match[4])], collapse=", ")))
-    result =  sum(sort(rolls)[as.numeric(1:match[4])])
-  } else {
-    result = NA
-    warning('dice could not be rolled')
+#' roll_one("1d6")
+#'
+roll_one <- function(roll){
+  for (r in roll_types) { # we try rolls patterns one by one
+    detected = stringr::str_detect(roll,r$pattern)
+    if (detected) { # when a pattern matches
+      match = stringr::str_match(roll, r$pattern)
+      result = r$compute(match) # it is possible to make the roll and apply its rules
+      return(result)
+    }
   }
-  return(result)
+  warning("This roll command is not recognized")
 }
+
+
+no_dice = list(pattern = "^\\d+$",
+               compute = function(match) {
+                 result = match[1]
+                 return(result)
+               })
+
+simple = list(pattern = "^(\\d+)[dD](\\d+)$",
+              compute = function(match) {
+                n = match[2]
+                sides = match[3]
+                rolls = sample(1:sides, n, replace = TRUE)
+                result = sum(rolls)
+              })
+
+keep_h = list(pattern = "^(\\d+)[dD](\\d+)[Hh](\\d+)",
+              compute = function(match) {
+                n = match[2]
+                sides = match[3]
+                kept = match[4]
+                rolls = sample(1:sides, n, replace = TRUE)
+                result =  sum(sort(rolls, decreasing = T)[1:as.numeric(kept)])
+              })
+
+keep_l = list(pattern = "^(\\d+)[dD](\\d+)[Ll](\\d+)",
+              compute = function(match) {
+                n = match[2]
+                sides = match[3]
+                kept = match[4]
+                rolls = sample(1:sides, n, replace = TRUE)
+                result =  sum(sort(rolls)[1:as.numeric(kept)])
+              })
+
+roll_types = list(
+  no_dice,
+  simple,
+  keep_h,
+  keep_l
+)
