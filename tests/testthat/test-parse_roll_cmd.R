1f080499a70c9d57dd9f50e227a0f1225e7b807b
Felix MIL <felixmil@outlook.com>
2020-07-01 00:30:03 +0200

    minor modification following refactoring in tested function

--- tests/testthat/test-parse_roll_cmd.R
+++ tests/testthat/test-parse_roll_cmd.R
@@ -1,7 +1,7 @@
 test_that("parsing simple roll cmd works", {
   parsed_cmd = parse_roll_cmd("1d6")
   expect_type(parsed_cmd, "list")
-  expect_equal(parsed_cmd$elements[1], "1d6")
+  expect_equal(parsed_cmd$dice[1], "1d6")
   expect_true(is.na(parsed_cmd$operators[1]))
 })
 
